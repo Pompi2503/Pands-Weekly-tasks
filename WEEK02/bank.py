941d090f589e9195de91fa64fafb12e3d9468d04
deepika g <gusain.deepika@gmail.com>
2025-02-16 23:31:43 +0000

    Week 02 bank.py changes

--- WEEK02/bank.py
+++ WEEK02/bank.py
@@ -1,14 +1,15 @@
 # Print an outcome of addition of two amounts in Euro denomination with upto two decimal points
 # Author Deepika
 # Input first amount
-amt1 = int(input ("Enter first amount in cents: "))
-amt2 = int(input ("Enter Second amount in cents: "))
+amt1 = int(input("Enter first amount in cents: ")) 
+# Input second amount
+amt2 = int(input("Enter Second amount in cents: "))
 # Calculte summation of two amounts in cents
-Total_Amount=amt1+amt2
+Total_Amount = amt1 + amt2
 # Convert Cents to Euros
 # First retrieve the Euro part
-Euro_part=Total_Amount//100
+Euro_part = Total_Amount//100
 # Retrieve cents part
-Cents_part=Total_Amount%100
+Cents_part = Total_Amount%100
 # Print total amount in Euro and cents upto 2 decimal places
 print(f"The total of two amounts is € {Euro_part}.{Cents_part:02d}")
