0d391cb9f96da5bd475c6efc17dc8022f27e9db1
deepika g <gusain.deepika@gmail.com>
2025-02-17 00:34:33 +0000

    WEEK03 started

---
+++ WEEK03/lab3.1.testTests.py
@@ -0,0 +1,13 @@
+# testTypes.py
+# Create variables for defined testType
+i = 3 # integer
+fl = 3.5 # float
+isa = True # Boolean
+memo = 'how now Brown Cow' # string
+lots =  ['l', 'o', 't', 's']  # list
+# Use type() to check the type of each variable
+print(f"i is of type: {type(i)} and value: {i}")
+print(f"fl is of type: {type(fl)} and value: {fl}")
+print(f"isa is of type: {type(isa)} and value: {isa}") 
+print(f"memo is of type: {type(memo)} and value: {memo}")
+print(f"lots is of type: {type(lots)} value: {lots}")
