f7a4d372007fb6b36c799fbbebf962a4a5abf7e3
deepika g <gusain.deepika@gmail.com>
2025-04-13 12:10:29 +0100

    WEEk06 not fully updated

--- WEEK 06/squareroot.py
+++ WEEK 06/squareroot.py
@@ -1,7 +1,9 @@
 # squreroort.py
 # Author Deepika Gusain
+
 # Program will create approximate square root using Newton's method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
 # Function is defined to calculate approx, square root of a positive floating number (https://www.w3schools.com/python/python_functions.asp)
+
 # function sqrt is defined with arument number and tolerance parameter which sets the level of precision
 def sqrt(number, tolerance=1e-6):
 
@@ -10,9 +12,11 @@ def sqrt(number, tolerance=1e-6):
 
 # It controls the loop in Newton's method and ensure that the iteration continues untill the approximatiin is accuateg enough
     while abs(result * result - number) > tolerance:
+
 # this ensures that new approximation is more accurate by averaging the current and alternative guess
 # It then calculates midpoint between the two estimates
         result = (result + number / result) / 2.0 
+        
 # Return the final apporximate result
     return result
 
