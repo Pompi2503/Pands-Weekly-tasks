57b8758e6ae8b5c59ed1848a16747a8b7258ae94
deepika g <gusain.deepika@gmail.com>
2025-04-13 12:09:03 +0100

    WEEk05 updated

--- WEEK 05-labs/weekday.py
+++ WEEK 05-labs/weekday.py
@@ -1,9 +1,14 @@
+# weekday.py 
+# Author Deepika Gusain
 # program to check whether today is a weekday or a weekend
-# this will import datetime module to access current date
+
+# this will import datetime module to access current date(https://www.w3schools.com/python/python_datetime.asp)
 import datetime
-# Retrive today's day of the week
+
+# Retrive today's day of the week(https://docs.python.org/3/library/datetime.html)
 today = datetime.datetime.today().weekday()
-# check if weekday or weekend
+
+# check if weekday or weekend (https://www.w3schools.com/python/python_conditions.asp)
 if today < 5:
     print("Yes, unfortunately today is a weekday")
 else:
