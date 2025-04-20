c3828c0ee0de8b1d6d8acdaeb53479e5b59252ba
deepika g <gusain.deepika@gmail.com>
2025-04-20 15:18:24 +0100

    ISo much of referecing i missed

--- WEEK 06/squareroot.py
+++ WEEK 06/squareroot.py
@@ -14,32 +14,34 @@ def sqrt(number, tolerance=1e-6):
     while abs(result * result - number) > tolerance:
 
 # this ensures that new approximation is more accurate by averaging the current and alternative guess
-# It then calculates midpoint between the two estimates
+# It then calculates midpoint between the two estimates (https://www.geeksforgeeks.org/newtons-method-in-machine-learning/)
         result = (result + number / result) / 2.0 
-        
-# Return the final apporximate result
+
+# Return the final apporximate result (https://docs.python.org/3/library/functional.html)
     return result
 
-# User input loop to ensure valid positive number 
+# User input loop to ensure valid positive number (https://pythonistaplanet.com/python-while-loop-examples/)
 while True:
 
-# try-except statement is used to ensure the program does not crash if the user enter the invalid input 
+# try-except statement is used to ensure the program does not crash if the user enter the invalid input
+#  (https://docs.python.org/3/tutorial/errors.html)
     try:
 # Take input
         user_input = float(input("Please enter a positive number: ")) 
 # If loop used checks  if input less than 0, it will proint error      
         if user_input <= 0:
             print("Error: Please enter a positive number.")
+
 # Prompt again to enter number
             continue
-# It will exit looping when valid input is entered
+# It will exit looping when valid input is entered (https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements)
         break
-# Try-expect part is part of exception handling
+# Try-expect part is part of exception handling to handling incorrect type conversion
     except ValueError:
         print("Error: Invalid input. Please enter a valid floating-point number.")
 
-# Calculate square root approximation
+# Calculate square root approximation (https://docs.python.org/3/library/math.html)
 result = sqrt(user_input)
 
-# Display output
+# Display output (https://docs.python.org/3/reference/lexical_analysis.html#f-strings)
 print(f"The square root of {user_input} is approx. {result:.1f}.")
